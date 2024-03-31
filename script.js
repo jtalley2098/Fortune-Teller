76a7441630fc69a6e1fbb01dafa20008376e022f
Jaa'Tal <jtalisman99@gmail.com>
2024-03-31 01:37:15 -0700

    added new music still doesn't work

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function ready(){
     
 
     const audio = document.querySelector('audio');
-    document.addEventListener("DOMContentLoaded", (ready) =>{
+    document.addEventListener("DOMContentLoaded", () =>{
         audio.play();
     });
 
@@ -26,16 +26,34 @@ getMessage.addEventListener('mouseout', ()=>{
 
 
 const randomFortuneArr = 
-["You will be eaten by a giant lizard if you go out.  Stay Home!", 
-"Love is in the air...always", 
-"You're frineds are cowards. Do not follow them. Take charge!", 
+["You will be eaten by the giant lizard i'm whipping up right now.", 
+"Love is in the air...wear the Elmo costume", 
+"Stop asking me for shit...I mean damn!", 
 "Fortune favors those who eat tacos, it despises those who eat pancakes.", 
 "When opportunity fails to answer kick the fucking door in!", 
 "That dick head at the grocery store isn't gonna beat himself up!",
 "Avoid the chilli. You'll thank me later :/.", 
 "Persevere. Soon you will be like Zues on Mt Olympus!", 
 "Quit your job, abandon your family, independant people are happy people.",
-"You are not your job or how much money you have in the bank!!!!"];
+"You are not your job or how much money you have in the bank.",
+"Congratulations! You've been selected to participate in the grand cosmic game of 'Figure It the fuck out'.",
+"Life's like a bowl of cosmic cereal... sometimes you find marshmallows, sometimes you find black holes.",
+"Just a heads up: Karma's on vacation. Enjoy the chaos.",
+"Jesus Christ has no idea who you bitches are.",
+"Soon you will be dead.  Bring snacks.",
+"When I'm bored I peer out from behind your eyes.",
+"Breaking news. I'm naming you center of the fucking universe...narcissist.",
+"Don't worry.  Hell is really not that bad.",
+"In the vast expanse of eternity, even stars fade into oblivion.",
+"Embrace the darkness within, for it mirrors the universe's cold indifference.",
+"The universe has a twisted sense of humor: it gives you existential dread as a birthday gift and then watches you scramble for meaning.",
+"Extraterrestials are real, they actualy like you, stop freaking out all the time.",
+"Go out for a pack of smokes and never come back.",
+"Observe how your mind makes meaning of events. This is the  greatest source of your suffering.",
+"The U.S. government planned 9/11.  Killing thousands of your own people.  'War makes money.'",
+"On holloween you're only getting candy corn.",
+"Christmas is a day to worship the (son) on the solstice.  Sun worshiper!",
+"Your misfortunes make me smile."];
 
 function generator () {
     let fortune = randomFortuneArr[Math.floor(Math.random()* randomFortuneArr.length)]
